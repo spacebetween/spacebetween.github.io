a377844e0be48ea5986fcbe1e2d0ed5b045721fd
Marcus Cooke <marcus.j.cooke@gmail.com>
2016-01-05 17:15:00 +0000

    Move things around.

--- assets/logic.js
+++ assets/logic.js
@@ -1,22 +1,19 @@
-$('.jsToggle').on('click', function(){
-	var target = $(this).data('toggle');
-	$(target).toggleClass('active bounceInDown');
-});
+'use strict';
 
-$(document).ready(function(){
-	function getRandom(min, max) {
-		return Math.floor(Math.random() * (max - min + 1)) + min;
-	}			
-	var canvas = document.getElementById('canvasContainer');
-	canvas.width = window.innerWidth;
+function getRandom(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function addStars(canvas) {
+	canvas.width = window.innerWidth,
 	canvas.height = window.innerHeight;
-	
-	var context = canvas.getContext('2d');
-	var stars = canvas.width / 2;
-	var colorrange = [0,60,240];
+
+	var context = canvas.getContext('2d'),
+	stars = canvas.width / 2,
+	colorrange = [0,60,240];
 
 	for (var i = 0; i < stars; i++) {
-		var x = Math.random() * canvas.offsetWidth;
+		var x = Math.random() * canvas.offsetWidth,
 		y = Math.random() * canvas.offsetHeight,
 		radius = Math.random() * 1.2,
 		hue = colorrange[getRandom(0,colorrange.length - 1)],
@@ -26,4 +23,14 @@ $(document).ready(function(){
 		context.fillStyle = "hsl(" + hue + ", " + sat + "%, 88%)";
 		context.fill();
 	}
+}
+
+$('.jsToggle').on('click', function(){
+	var target = $(this).data('toggle');
+	$(target).toggleClass('active bounceInDown');
 });
+
+var canvas = document.getElementById('canvasContainer');
+if (canvas) {
+	addStars(canvas);
+}
